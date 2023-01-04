c6933935c24ff4128a8cacc31a4081618154da5b
LJD <71216465+M0ULui@users.noreply.github.com>
2023-01-05 00:59:11 +0800

    Add files via upload

--- tools/wordlist_generator.py
+++ tools/wordlist_generator.py
@@ -30,7 +30,7 @@ class Pydictor(HackingTool):
         os.system("cd pydictor/ && chmod +x pydictor.py && python pydictor.py")
         os.system ("echo '\033[1;32m---------------------------------------\033[m';")
         subinput = input("\033[33;5m Pydictor => \033[0m")
-        subprocess.run(["cd pydictor/ && chmod +x pydictor.py &&",subinput])
+        subprocess.run(["cd /home/afoht/pydictor/ && chmod +x pydictor.py &&",subinput])
 
 class WlCreator(HackingTool):
     TITLE = "WordlistCreator"
