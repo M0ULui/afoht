ec626501f2e917ddba3002179e944bb7309b5134
LJD <71216465+M0ULui@users.noreply.github.com>
2023-01-05 03:39:35 +0800

    Add files via upload

--- tools/webattack.py
+++ tools/webattack.py
@@ -24,9 +24,9 @@ class Sublist3r(HackingTool):
 
     def run(self):
         os.system("cd Sublist3r && python3 sublist3r.py -h")
-        #os.system("echo '\033[1;32m---------------------------------------\033[m'")
+        os.system("echo '\033[1;32m---------------------------------------\033[m'")
         subinput = input("\033[33;5m Sublist3r >>\033[0m")
-        os.system(["cd Sublist3r &&",subinput])
+        os.system(["cd /home/afoht/Sublist3r;",subinput])
 
 
 class CheckURL(HackingTool):
