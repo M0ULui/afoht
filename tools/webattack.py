ee4184f5d329a5816b72a9989067712fef6b89db
LJD <71216465+M0ULui@users.noreply.github.com>
2023-01-05 04:13:08 +0800

    Add files via upload

--- tools/webattack.py
+++ tools/webattack.py
@@ -26,7 +26,7 @@ class Sublist3r(HackingTool):
         os.system("cd Sublist3r && python3 sublist3r.py -h")
         subinput = input('Enter URL >>')
         os.system("cd Sublist3r")
-        subprocess.run(['python3', 'sublist3r.py', '-d', subinput])
+        subprocess.run('cd Sublist3r && python3 sublist3r.py -d', subinput, shell=True)
 
 
 class CheckURL(HackingTool):
