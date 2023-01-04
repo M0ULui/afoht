346e5cde8669e896feb0f16921776fe37b5264d8
LJD <71216465+M0ULui@users.noreply.github.com>
2023-01-05 04:21:28 +0800

    Add files via upload

--- tools/webattack.py
+++ tools/webattack.py
@@ -42,7 +42,7 @@ class CheckURL(HackingTool):
         os.system("cd checkURL && python3 checkURL.py --help")
         os.system ("echo '\033[1;32m---------------------------------------\033[m';")
         subinput = input("\033[33;5m Enter Url >>\033[0m")
-        os.system(["cd checkURL && sudo python3 checkURL.py --url", subinput])
+        os.system(["cd checkURL && python3 checkURL.py --url" + subinput])
 
 
 class Dirb(HackingTool):
