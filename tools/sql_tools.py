40f12c09a3d02287df91b75303256e6adb851b4a
LJD <71216465+M0ULui@users.noreply.github.com>
2023-01-05 02:20:21 +0800

    Add files via upload

--- tools/sql_tools.py
+++ tools/sql_tools.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import os
+
 from core import HackingTool
 from core import HackingToolsCollection
 
@@ -34,12 +36,18 @@ class NoSqlMap(HackingTool):
 class Sqlninja(HackingTool):
     TITLE = "Sqlnijnja"
     DESCRIPTION = "Sqlninja is a tool targeted to exploit SQL Injection vulnerabilities" \
-                  "on a web application that uses Microsoft SQL Server as its back-end." 
+                  "on a web application that uses Microsoft SQL Server as its back-end.\n"\
+                    "[!] e.g. sqlninja -m t -f /root/sqlninja.conf" 
     INSTALLED_SIZE = "10.51 MB"
     INSTALL_COMMANDS = [
         "sudo apt install sqlninja"]
     RUN_COMMANDS = ["sudo sqlninja"]
     PROJECT_URL = "https://www.kali.org/tools/sqlninja/"
+
+    def run(self):
+        os.system ("echo '\033[1;32m---------------------------------------\033[m';")
+        subinput = input("\033[33;5m Sqlninja => \033[0m")
+        os.system(subinput)
 
 class SqlInjectionTools(HackingToolsCollection):
     TITLE = "SQL Injection Tools"
