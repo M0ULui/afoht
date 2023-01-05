9479134bc2a0e7029408510cb87a7201889dd01d
LJD <71216465+M0ULui@users.noreply.github.com>
2023-01-06 06:31:33 +0800

    Add files via upload

---
+++ tools/afoht.py
@@ -0,0 +1,113 @@
+##!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# Version 1.1.0
+import os
+import webbrowser
+from platform import system
+from time import sleep
+
+from core import HackingToolsCollection
+from tools.ddos import DDOSTools
+from tools.forensic_tools import ForensicTools
+from tools.information_gathering_tools import InformationGatheringTools
+from tools.payload_creator import PayloadCreatorTools
+from tools.phising_attack import PhishingAttackTools
+from tools.reverse_engineering import ReverseEngineeringTools
+from tools.sql_tools import SqlInjectionTools
+from tools.webattack import WebAttackTools
+from tools.wordlist_generator import WordlistGeneratorTools
+from tools.password_cracking import PasswordCrackingTools
+from tools.management import *
+
+logo = """\033[36m 
+    _    _     _          _____ ___  ____        ___  _   _ _____ 
+   / \  | |   | |        |  ___/ _ \|  _ \      / _ \| \ | | ____|
+  / _ \ | |   | |   _____| |_ | | | | |_) |____| | | |  \| |  _|  
+ / ___ \| |___| |__|_____|  _|| |_| |  _ <_____| |_| | |\  | |___ 
+/_/   \_\_____|_____|    |_|   \___/|_| \_\     \___/|_| \_|_____|
+\033[35m
+ _   _    _    ____ _  _____ _   _  ____    _____ ___   ___  _     
+| | | |  / \  / ___| |/ /_ _| \ | |/ ___|  |_   _/ _ \ / _ \| |    
+| |_| | / _ \| |   | ' / | ||  \| | |  _     | || | | | | | | |    
+|  _  |/ ___ \ |___| . \ | || |\  | |_| |    | || |_| | |_| | |___ 
+|_| |_/_/   \_\____|_|\_\___|_| \_|\____|    |_| \___/ \___/|_____|  
+
+    \033[34m[✔]    https://github.com/M0ULui/afoht     [✔]
+    \033[34m[✔]              Version 1.3               [✔]
+    \033[91m[X] Please Don't Use For illegal Activity  [X]
+\033[97m """
+
+all_tools = [
+    InformationGatheringTools(),
+    WordlistGeneratorTools(),
+    SqlInjectionTools(),
+    PhishingAttackTools(),
+    WebAttackTools(),
+    ForensicTools(),
+    PayloadCreatorTools(),
+    ReverseEngineeringTools(),
+    DDOSTools(),
+    PasswordCrackingTools(),
+    Management()
+]
+
+
+
+
+class AllTools(HackingToolsCollection):
+    TITLE = "All tools"
+    TOOLS = all_tools
+
+    def show_info(self):
+        print(logo + '\033[0m \033[97m')
+
+
+if __name__ == "__main__":
+    try:
+        if system() == 'Linux':
+            fpath = "/home/AFOHTpath.txt"
+            if not os.path.exists(fpath):
+                os.system('clear')
+                # run.menu()
+                print("""
+                        [@] Set Path (All your tools will be installed in that directory)
+                        [1] Manual 
+                        [2] Default
+                """)
+                choice = input("AFOHT =>> ")
+
+                if choice == "1":
+                    print("Manual Path will be available in the near future!")
+                    Manpath = "/home/afoht/"
+                    with open(fpath, "w") as f:
+                        f.write(Manpath)
+                    print(f"Successfully Set to Default Path: {Manpath}")
+                elif choice == "2":
+                    autopath = "/home/afoht/"
+                    with open(fpath, "w") as f:
+                        f.write(autopath)
+                    print(f"Your Default Path Is: {autopath}")
+                    sleep(3)
+                else:
+                    print("Try Again..!!")
+                    exit(0)
+
+            with open(fpath) as f:
+                archive = f.readline()
+                if not os.path.exists(archive):
+                    os.mkdir(archive)
+                os.chdir(archive)
+                AllTools().show_options()
+
+        # If not Linux and probably Windows
+        elif system() == "Windows":
+            print(
+                r"\033[91m Please Run This Tool On A Debian System For Best Results\e[00m"
+            )
+            sleep(2)
+        else:
+            print("Please Check Your System and Try Again ...")
+
+    except KeyboardInterrupt:
+        print("\nExiting ..!!!")
+        sleep(2)
---
+++ tools/install.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+set -e
+
+clear
+
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[92m'
+YELLOW='\e[33m'
+ORANGE='\e[93m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+
+echo -e "${CYAN} "
+echo ""
+echo "    _    _     _          _____ ___  ____        ___  _   _ _____ ";
+echo "   / \  | |   | |        |  ___/ _ \|  _ \      / _ \| \ | | ____|";
+echo "  / _ \ | |   | |   _____| |_ | | | | |_) |____| | | |  \| |  _|  ";
+echo " / ___ \| |___| |__|_____|  _|| |_| |  _ <_____| |_| | |\  | |___ ";
+echo "/_/   \_\_____|_____|    |_|   \___/|_| \_\     \___/|_| \_|_____|";
+echo "";
+echo -e "${PURPLE}"
+echo " _   _    _    ____ _  _____ _   _  ____    _____ ___   ___  _ ";
+echo "| | | |  / \  / ___| |/ /_ _| \ | |/ ___|  |_   _/ _ \ / _ \| |    ";
+echo "| |_| | / _ \| |   | ' / | ||  \| | |  _     | || | | | | | | |    ";
+echo "|  _  |/ ___ \ |___| . \ | || |\  | |_| |    | || |_| | |_| | |___ ";
+echo "|_| |_/_/   \_\____|_|\_\___|_| \_|\____|    |_| \___/ \___/|_____|";
+echo -e "${WHITE}----------------------------------------------------------------------"
+echo -e "${BLUE}     [+] https://github.com/M0ULui/afoht [+] ${NC}"
+echo -e "${RED}       [!] This Tool Must Run As ROOT [!]${NC}\n"
+echo -e "${WHITE}----------------------------------------------------------------------"
+echo -e ${ORANGE}                "Available OS Options : \n"
+echo -e "${WHITE}              [1] Kali Linux"
+echo -e "${WHITE}              [0] Exit "
+echo -e "${WHITE}----------------------------------------------------------------------"
+echo -n -e "${YELLOW} AFOHT >> "
+read choice
+INSTALL_DIR="/usr/share/doc/afoht"
+BIN_DIR="/usr/bin/"
+if [ $choice == 1 ]; then
+	echo "[*] Ensuring Internet Connection is Fine.."
+	wget -q --tries=10 --timeout=20 --spider https://google.com
+	if [ $? == 0 ]; then
+        echo -e ${BLUE}"[✔] Initializing ... "
+        if [ $choice == 1 ]; then
+            sudo apt-get update -y && apt-get upgrade -y
+            sudo apt-get install python3-pip -y
+        fi
+
+	    echo "[✔] Checking directories..."
+	    if [ -d "$INSTALL_DIR" ]; then
+	        echo "[!] A Directory for All-For-One Hacking Tool Was Found. Do You Want To Replace It ? [y/n]:" ;
+	        read input
+	        if [ "$input" = "y" ]; then
+	            sudo rm -R "$INSTALL_DIR"
+	        else
+	            exit
+	        fi
+	    fi
+
+        echo "[✔] Installing ...\n";
+        sudo git clone https://github.com/M0ULui/afoht.git "$INSTALL_DIR";
+        echo "#!/bin/bash
+        python3 $INSTALL_DIR/afoht.py" '${1+"$@"}' > afoht;
+        sudo chmod +x afoht;
+        sudo cp afoht /usr/bin/ && rm afoht;
+
+        echo "\n[+] Trying to install The Prerequisites ..."
+        if [ $choice == 1 ]; then
+            sudo pip3 install lolcat flask requests
+            sudo apt-get install -y figlet
+            apt-get install -y boxes
+        fi
+
+	else
+		  echo -e $RED "!! Unable To Proceed, Internet Connection is Weak !!"
+	fi
+
+    if [ -d "$INSTALL_DIR" ]; then
+        echo "";
+        echo "[✔] Successfuly Installed !!!";
+        echo -e $ORANGE "       [✔]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[✔]"
+        echo            "       [✔]                                                             [✔]"
+        echo -e $ORANGE "       [✔]    ✔✔✔ Proceed by Typing (afoht) in the Terminal ✔✔✔     [✔]"
+        echo            "       [✔]                                                             [✔]"
+        echo -e $ORANGE "       [✔]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[✔]"
+    else
+        echo "[✘] Installation Failed, Please Try Again. [✘]";
+        exit
+    fi
+
+elif [ $choice == 0] && [ $choice != 1 ] ; then
+    echo -e $RED "[+] Thank You! [+] "
+    exit
+else
+    echo -e $RED "[!] Please Select A Valid Option [!]"
+fi
